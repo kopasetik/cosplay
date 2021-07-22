2d23b486f3683008823e0e365863c42d7b826c7d
Wale O <dev@screenverve.com>
2021-07-21 23:12:25 -0700

    add masonry
    the next step will be to make the widths of the masonry tiles uniform.
    it makes for a better look. also, i need to remove the container
    surrounding the boxes. but i have to make sure that there is only one
    parent element in the component. and i need to make the masonry box the
    whole width of the window... more or less.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Box from '@material-ui/core/Box';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ImageList from '@material-ui/core/ImageList';
+import ImageListItem from '@material-ui/core/ImageListItem';
+import Container from '@material-ui/core/Container';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+export default function ButtonAppBar() {
+	const getRandomArbitrary = (min, max) => (
+		Math.floor(Math.random() * (max - min) + min)
+	)
+	const getDimensions = () => (
+		{
+			x: getRandomArbitrary(50, 300),
+			y: getRandomArbitrary(80, 350)
+		})
+	const getPlaceholderUrl = ({x,y})=> (`https://via.placeholder.com/${x}x${y}.png`)
+
+	return (
+		<Container>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Cosplays
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+		</Box>
+		<Box sx={{ width: 500, height: 450, overflowY: 'scroll' }}>
+	  <ImageList variant="masonry" cols={3} gap={8}>
+  		{Array(15).fill('').map((item, idx) => (
+    			<ImageListItem key={idx}>
+      				<img
+        			src={getPlaceholderUrl(getDimensions())}
+				alt={`idx`}
+        			loading="lazy"
+      				/>
+    			</ImageListItem>
+ 		 ))}
+	</ImageList>
+    </Box>
+		</Container>
   );
 }
 
-export default App;
